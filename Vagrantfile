2cc90595dda418330c148923eb7df603f6f2e28d
kpsysops <kp.sysops@gmail.com>
2021-05-03 16:41:20 +0200

    adding config files for DNS/bind9

--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,8 @@ Vagrant.configure("2") do |config|
         dns.vm.hostname = "dns.example.com"
         dns.vm.box = "generic/centos7"
         dns.vm.network "private_network", ip: "10.10.10.2"
-        dns.vm.provision "shell", path: ./setupDNS.sh
+        #dns.vm.provision "shell", path: setupDns.sh
+        dns.vm.synced_folder ".", "/vagrant", type: "rsync"
         dns.vm.provider "virtualbox" do |vb|
             vb.cpus = "1"
             vb.memory = "1024"
--- setupDns.sh
+++ setupDns.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 echo "Instaling DNS resoultion for example.com with bind9" | logger 
+#Install dns packages
 sudo yum -y install bind bind-utils 
+#Copy config files from vagrant dir (no addional changes needed) 
+
+
+#Start and enable dns daemon (bind9)
+sudo systemctl --now enable named
+sudo firewall-cmd --permanent --zone=public --add-service=dns
+sudo firewall-cmd --reload
+
+#Adding DNS IP to DNS server
+ETH0=$(sudo nmcli connection show | grep eth0 | cut -d ' ' -f 4)
+ETH1=$(sudo nmcli connection show | grep eth1 | cut -d ' ' -f 4)
+sudo nmcli connection modify $ETH0 ipv4.dns-priority 10
+sudo nmcli connection modify $ETH1 ipv4.dns-priority 1
+sudo nmcli connection modify $ETH1  ipv4.dns 10.10.10.2
+sudo nmcli connection modify $ETH1  ipv4.dns-search example.com
+sudo nmcli connection up $ETH1 
+sudo nmcli connection up $ETH0 
+
+
+
 
